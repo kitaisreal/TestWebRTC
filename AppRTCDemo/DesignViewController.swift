dfa6b27c9bedacc59396fcc2df17cb71ae2acbb7
kitaisreal <kitaetoya@gmail.com>
2018-01-22 22:04:35 +0300

    Add Video Call Full module

---
+++ AppRTCDemo/DesignViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DesignViewController.swift
+//  AppRTCDemo
+//
+//  Created by Maksim Kita on 1/22/18.
+//  Copyright © 2018 Maksim Kita. All rights reserved.
+//
+
+import UIKit
+
+class DesignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AppRTCDemo/Model/Constants/AnimationConstants.swift
@@ -0,0 +1,9 @@
+//
+//  AnimationConstants.swift
+//  AppRTCDemo
+//
+//  Created by Maksim Kita on 1/22/18.
+//  Copyright © 2018 Maksim Kita. All rights reserved.
+//
+
+import Foundation
