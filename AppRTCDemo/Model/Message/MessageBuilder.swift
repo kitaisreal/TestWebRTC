782cc7d0a5b6871562d8a89672cf56d764cbe79d
kitaisreal <kitaetoya@gmail.com>
2018-01-23 18:12:09 +0300

    Start working on next release

---
+++ AppRTCDemo/Model/Message/MessageBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  MessageBuilder.swift
+//  AppRTCDemo
+//
+//  Created by Maksim Kita on 1/23/18.
+//  Copyright © 2018 Maksim Kita. All rights reserved.
+//
+
+import Foundation
---
+++ AppRTCDemo/Model/Message/MessageTypes.swift
@@ -0,0 +1,9 @@
+//
+//  MessageTypes.swift
+//  AppRTCDemo
+//
+//  Created by Maksim Kita on 1/23/18.
+//  Copyright © 2018 Maksim Kita. All rights reserved.
+//
+
+import Foundation
